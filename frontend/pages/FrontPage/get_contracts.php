5b2fe44f6d3b842bfd6b32b0cad4b98e905e3a47
Dodge <60789019+Dodge77860@users.noreply.github.com>
2024-12-02 22:33:05 -0500

    chanted connection

--- frontend/pages/FrontPage/get_contracts.php
+++ frontend/pages/FrontPage/get_contracts.php
@@ -2,18 +2,7 @@
 session_start(); // Start the session
 
 // Database connection settings
-$servername = "localhost";
-$username = "root"; 
-$password = ""; 
-$dbname = "caregiver_web";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include('../../../backend/conn.php');
 
 // Check if the user is logged in (assuming MID is set in the session)
 if (isset($_SESSION['MID'])) {
